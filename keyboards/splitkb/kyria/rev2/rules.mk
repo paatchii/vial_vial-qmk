7514cae9ac421a58f9089354ad2ef195df2ca19d
Patchii <50637577+paatchii@users.noreply.github.com>
2024-04-02 22:58:34 +0200

    add own files

--- keyboards/splitkb/kyria/rev2/rules.mk
+++ keyboards/splitkb/kyria/rev2/rules.mk
@@ -9,5 +9,5 @@ AUDIO_ENABLE = no          # Audio output
 
 OLED_ENABLE = yes          # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes    # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
